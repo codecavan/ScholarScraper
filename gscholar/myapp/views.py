a781667d2aebcf2e86746a94e9a3c950f9381aa6
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2024-01-19 23:19:35 +0330

    .

--- gscholar/myapp/views.py
+++ gscholar/myapp/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-from myapp.scraper.main import getAuthorProfileData
+from myapp.scraper.main import scraper
+
+
+# author_urls = ['https://scholar.google.com/citations?user=PT8wxO8AAAAJ&hl=en']
 
 
 def home(request):
-    data = getAuthorProfileData()
-    print(data)
+    return render(request, "home.html")
+
+
+def articles(request):
+    query = request.GET.get('urls', '')
+
+    if query.strip() == '':
+        return render(request, "articles.html", {"articles": []})
+
+    raw_urls = query.split(' ')
+    urls = [url.strip() for url in raw_urls]
+
+    print(urls)
+    data = scraper(urls) if len(urls) > 0 else []
     return render(request, "articles.html", {"articles": data})

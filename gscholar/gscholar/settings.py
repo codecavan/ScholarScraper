2221ce94f2f3ea6f1888474c603d8745f3f60f7e
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2024-01-19 23:49:22 +0330

    remove htmx

--- gscholar/gscholar/settings.py
+++ gscholar/gscholar/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp',
-    'django_htmx',
 ]
 
 MIDDLEWARE = [
@@ -49,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "django_htmx.middleware.HtmxMiddleware",
 ]
 
 ROOT_URLCONF = 'gscholar.urls'

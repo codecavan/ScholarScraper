8a6bb23e39d8272ed6c2cb669e6d5ac323cd348d
Amir H. Khanjani <72540492+ahkhanjani@users.noreply.github.com>
2024-01-19 21:14:53 +0330

    improve the code

--- scholar_scraper_v2.py
+++ scholar_scraper_v2.py
@@ -11,20 +11,23 @@ user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 headers = {"User-Agent": user_agent}
 
 
-def process_author_profile(url):
+def extract_user_id(url):
+    pattern = r'user=([^&]+)'
+    match = re.search(pattern, url)
+
+    if match:
+        return match.group(1)
+
+
+def process_profile(url):
     try:
-        profile_data = get_author_profile_data(url)
-
-        # New code to extract articles using parsel
         user_id = extract_user_id(url)
-        print("User ID is: " + user_id)
+        print("User ID: " + user_id)
 
-        citation_data = get_citation_data(user_id)
-        # Convert data to DataFrames
-        df_citation_data = pd.DataFrame(citation_data['cited_by'])
-        # Save data to CSV
-        df_citation_data.to_csv(
-            profile_data['name']+'_Profile'+'.csv', index=False)
+        profile_data = get_profile_data(url)
+        auhor_name = profile_data['name']
+
+        process_citation_data(user_id, auhor_name)
 
         articles = get_articles(user_id)
         return articles
@@ -33,17 +36,7 @@ def process_author_profile(url):
         print(e)
 
 
-def extract_user_id(url):
-    pattern = r'user=([^&]+)'
-    match = re.search(pattern, url)
-
-    if match:
-        return match.group(1)
-
-    return None
-
-
-def get_author_profile_data(url):
+def get_profile_data(url):
     response = requests.get(url, headers=headers)
     # print(response.status_code)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -108,12 +101,17 @@ def get_articles(user_id):
         if selector.css('.gsc_a_e').get():
             if len(articles) > 1 and articles[-1]['position'] == params['cstart'] + 1:
                 articles.pop()
-
             break
-
         params['cstart'] += 100
 
     return articles
+
+
+def process_citation_data(user_id, author_name):
+    citation_data = get_citation_data(user_id)
+
+    df_citation_data = pd.DataFrame(citation_data['cited_by'])
+    df_citation_data.to_csv(author_name+"'s Profile.csv", index=False)
 
 
 def get_citation_data(user_id):
@@ -185,16 +183,12 @@ def get_author_info(user_id):
     return author_info
 
 
-url_list = ['https://scholar.google.com/citations?user=PT8wxO8AAAAJ&hl=en',
-            'https://scholar.google.com/citations?user=T-CSbJgAAAAJ&hl=en&oi=hraa', 'https://scholar.google.com/citations?user=2LlnnZUAAAAJ&hl=en&oi=ao']
-
-
-def main():
+def main(urls):
     merged_articles = []
 
-    for list_item in url_list:
-        print('Processing: ', list_item)
-        author_articles = process_author_profile(list_item)
+    for url in urls:
+        print('Processing: ', url)
+        author_articles = process_profile(url)
         merged_articles += author_articles
         print('Finished!'+'\n')
 
@@ -203,4 +197,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    author_urls = ['https://scholar.google.com/citations?user=PT8wxO8AAAAJ&hl=en',
+                   'https://scholar.google.com/citations?user=T-CSbJgAAAAJ&hl=en&oi=hraa',
+                   'https://scholar.google.com/citations?user=2LlnnZUAAAAJ&hl=en&oi=ao']
+
+    main(author_urls)
